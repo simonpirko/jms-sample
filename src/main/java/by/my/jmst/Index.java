7e6bf3322b34a3b34a9733bdd9e6545c69ca09d5
Simon Pirko <sipi7gm@gmail.com>
2019-04-25 01:24:48 +0300

    ref#2 add ejb spec.

--- src/main/java/by/my/jmst/Index.java
+++ src/main/java/by/my/jmst/Index.java
@@ -1,16 +1,15 @@
 package by.my.jmst;
 
 import javax.ejb.EJB;
-import javax.inject.Inject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/p")
+@WebServlet(urlPatterns = "/producer")
 public class Index extends HttpServlet {
 
-    @Inject
+    @EJB
     private Producer producer;
 
     @Override
--- src/main/java/by/my/jmst/Producer.java
+++ src/main/java/by/my/jmst/Producer.java
@@ -3,10 +3,7 @@ package by.my.jmst;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.jms.JMSConnectionFactory;
-import javax.jms.JMSContext;
-import javax.jms.JMSProducer;
-import javax.jms.Topic;
+import javax.jms.*;
 
 @Stateless
 public class Producer {
@@ -15,7 +12,7 @@ public class Producer {
     private JMSContext jmsContext;
 
     @Resource(lookup = "jms/Topic")
-    private Topic topic;
+    private Destination topic;
 
     public void send() {
         JMSProducer producer = jmsContext.createProducer();
