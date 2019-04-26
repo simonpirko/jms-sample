5a6d378b1b1c77f952234fd0e0326f8b0d8c5dfa
Simon Pirko <sipi7gm@gmail.com>
2019-04-26 13:29:54 +0300

    ref#4 add sess. context

--- jmsconsumer/src/main/java/by/my/consjms/ConsumerTwo.java
+++ jmsconsumer/src/main/java/by/my/consjms/ConsumerTwo.java
@@ -1,6 +1,8 @@
 package by.my.consjms;
 
+import javax.ejb.EJB;
 import javax.ejb.MessageDriven;
+import javax.ejb.SessionContext;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -9,13 +11,16 @@ import javax.jms.TextMessage;
 @MessageDriven(mappedName = "jms/Topic")
 public class ConsumerTwo implements MessageListener {
 
+    @EJB
+    private SessionContext sessionContext;
+
     @Override
     public void onMessage(Message message) {
         TextMessage message1 = (TextMessage) message;
         try {
             System.out.println(this.getClass().getName() + " - " + message1.getText());
         } catch (JMSException e) {
-            e.printStackTrace();
+            sessionContext.setRollbackOnly();
         }
     }
 }
